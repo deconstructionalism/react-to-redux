8bf429ca708e75fc5067e8e130b62a4c61f90a42
deconstructionalism <deconstructionalism@gmail.com>
2019-05-17 13:57:48 -0400

    Refactoring for Redux - Step 3
    - import `createStore` and set store in `Provider`

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
 import './index.css'
 
+const store = createStore()
+
 ReactDOM.render(
-  <Provider>
+  <Provider store={ store }>
     <App />
   </Provider>
 , document.getElementById('root'))
